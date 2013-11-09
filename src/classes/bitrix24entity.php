8d03d8370af63bcd902214babc2c185a55d1c0b2
Mesilov Maxim <mesilov.maxim@gmail.com>
2013-11-09 14:22:03 +0400

    Add a base class Bitrix24Entity

---
+++ src/classes/bitrix24entity.php
@@ -0,0 +1,23 @@
+<?php
+namespace Bitrix24;
+
+/**
+ * Class Bitrix24Entity
+ */
+abstract class Bitrix24Entity
+{
+	const ITEMS_PER_PAGE_LIMIT = 50;
+
+	/**
+	 * @var Bitrix24
+	 */
+	public $client = null;
+
+	/**
+	 * @param $client Bitrix24
+	 */
+	public function __construct($client)
+	{
+		$this->client = $client;
+	}
+}
