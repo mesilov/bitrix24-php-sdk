2ed0f9edf44e4811457ceecbec2c4860de2a55f9
Mesilov Maxim <mesilov.maxim@gmail.com>
2014-08-17 02:23:24 +0400

    Add class Bitrix24\Presets\Scope for Bitrix24 scope constants

---
+++ src/presets/scope.php
@@ -0,0 +1,16 @@
+<?php
+namespace Bitrix24\Presets;
+
+class Scope
+{
+	const DEPARTMENT = 'department';
+	const CRM = 'crm';
+	const CALENDAR = 'calendar';
+	const USER = 'user';
+	const ENTITY = 'entity';
+	const TASK = 'task';
+	const TASKS_EXTENDED = 'tasks_extended';
+	const IM = 'im';
+	const LOG = 'log';
+	const SONET_GROUP = 'sonet_group';
+}
