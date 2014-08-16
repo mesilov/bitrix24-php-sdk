65728e1e119b9af75d3f77ea32ff0ed23746f704
Mesilov Maxim <mesilov.maxim@gmail.com>
2014-08-17 02:22:17 +0400

    remove require_once instructions in main class

--- src/bitrix24.php
+++ src/bitrix24.php
@@ -1,16 +1,5 @@
 <?php
 namespace Bitrix24;
-require_once("bitrix24exception.php");
-require_once("classes/bitrix24entity.php");
-require_once("classes/tasks.php");
-require_once("classes/task.php");
-require_once("classes/lead.php");
-require_once("classes/contact.php");
-require_once("classes/bitrix24application.php");
-require_once("classes/sonetgroup.php");
-require_once("classes/user.php");
-require_once("classes/events.php");
-
 class Bitrix24
 {
 	/**
