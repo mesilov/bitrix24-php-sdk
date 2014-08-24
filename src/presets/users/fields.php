70e1af6902bff9d5737aab66e1c7ef9bfb9c0493
Mesilov Maxim <mesilov.maxim@gmail.com>
2014-08-24 18:39:52 +0400

    Add class  Bitrix24\Presets\Users\Fields for Bitrix24 users fields

---
+++ src/presets/users/fields.php
@@ -0,0 +1,45 @@
+<?php
+namespace Bitrix24\Presets\Users;
+
+/**
+ * Class Fields
+ * @link http://dev.1c-bitrix.ru/rest_help/users/user_fields.php
+ * @package Bitrix24\Presets\Users
+ */
+class Fields
+{
+	const ID 					= 'ID';
+	const ACTIVE				= 'ACTIVE';
+	const EMAIL					= 'EMAIL';
+	const NAME					= 'NAME';
+	const LAST_NAME				= 'LAST_NAME';
+	const SECOND_NAME			= 'SECOND_NAME';
+	const PERSONAL_GENDER		= 'PERSONAL_GENDER';
+	const PERSONAL_PROFESSION	= 'PERSONAL_PROFESSION';
+	const PERSONAL_WWW			= 'PERSONAL_WWW';
+	const PERSONAL_BIRTHDAY		= 'PERSONAL_BIRTHDAY';
+	const PERSONAL_PHOTO		= 'PERSONAL_PHOTO';
+	const PERSONAL_ICQ			= 'PERSONAL_ICQ';
+	const PERSONAL_PHONE		= 'PERSONAL_PHONE';
+	const PERSONAL_FAX			= 'PERSONAL_FAX';
+	const PERSONAL_MOBILE		= 'PERSONAL_MOBILE';
+	const PERSONAL_PAGER		= 'PERSONAL_PAGER';
+	const PERSONAL_STREET		= 'PERSONAL_STREET';
+	const PERSONAL_CITY			= 'PERSONAL_CITY';
+	const PERSONAL_STATE		= 'PERSONAL_STATE';
+	const PERSONAL_ZIP			= 'PERSONAL_ZIP';
+	const PERSONAL_COUNTRY		= 'PERSONAL_COUNTRY';
+	const WORK_COMPANY			= 'WORK_COMPANY';
+	const WORK_POSITION			= 'WORK_POSITION';
+	const DEPARTMENT			= 'UF_DEPARTMENT';
+	const INTERESTS				= 'UF_INTERESTS';
+	const SKILLS				= 'UF_SKILLS';
+	const WEB_SITES				= 'UF_WEB_SITES';
+	const XING					= 'UF_XING';
+	const LINKEDIN				= 'UF_LINKEDIN';
+	const FACEBOOK				= 'UF_FACEBOOK';
+	const TWITTER				= 'UF_TWITTER';
+	const SKYPE					= 'UF_SKYPE';
+	const DISTRICT				= 'UF_DISTRICT';
+	const PHONE_INNER			= 'UF_PHONE_INNER';
+}
