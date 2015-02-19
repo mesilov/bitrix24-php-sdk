38c3f644c3e5eba49d5f124fa17be9ce0ff71f21
Mesilov Maxim <mesilov.maxim@gmail.com>
2015-02-20 00:42:14 +0300

    Add some presets for main entity

--- src/presets/main.php
+++ src/presets/main.php
@@ -20,5 +20,14 @@ class Main
 	const ORDER_BY_DESC = 'DESC';
 
 	const TOTAL ='total';
+
 	const RESULT = 'result';
+
+	const ACCESS_TOKEN = 'access_token';
+	const EXPIRES_IN = 'expires_in';
+	const SCOPE = 'scope';
+	const STATUS = 'status';
+	const REFRESH_TOKEN = 'refresh_token';
+	const DOMAIN = 'domain';
+	const MEMBER_ID = 'member_id';
 }
