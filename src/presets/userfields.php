ab475e4cee3c292d466008d4568b182b0f8c6030
Mesilov Maxim <mesilov.maxim@gmail.com>
2015-03-03 22:53:11 +0300

    Add presets for user fields data type structure

---
+++ src/presets/userfields.php
@@ -0,0 +1,15 @@
+<?php
+namespace Bitrix24\Presets;
+
+class UserFields
+{
+	const TYPE = 'type';
+	const IS_REQUIRED = 'isRequired';
+	const IS_READ_ONLY = 'isReadOnly';
+	const IS_IMMUTABLE = 'isImmutable';
+	const IS_MULTIPLE = 'isMultiple';
+	const IS_DYNAMIC = 'isDynamic';
+	const LIST_LABEL = 'listLabel';
+	const FORM_LABEL = 'formLabel';
+	const FILTER_LABEL = 'filterLabel';
+}
