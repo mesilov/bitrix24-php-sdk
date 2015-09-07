aca594481836506a8ac32ac1c9677159f4b04e66
mesilov <mesilov.maxim@gmail.com>
2015-09-08 00:05:49 +0300

    add preset lang

---
+++ src/presets/lang.php
@@ -0,0 +1,41 @@
+<?php
+namespace Bitrix24\Presets;
+/**
+ * Class Lang
+ * @package Bitrix24\Presets
+ */
+class Lang
+{
+	/**
+	 * @var string russian
+	 */
+	const RUSSIAN = 'ru';
+	/**
+	 * @var string english
+	 */
+	const ENGLISH = 'en';
+	/**
+	 * @var string german
+	 */
+	const GERMAN = 'de';
+	/**
+	 * @var string ukrainian
+	 */
+	const UKRAINIAN = 'ua';
+	/**
+	 * @var string spanish
+	 */
+	const SPANISH = 'la';
+	/**
+	 * @var string chinese traditional
+	 */
+	const CHINESE_TRADITIONAL = 'tc';
+	/**
+	 * @var string chinese simplified
+	 */
+	const CHINESE_SIMPLIFIED = 'sc';
+	/**
+	 * @var string portuguese
+	 */
+	const PORTUGUESE = 'br';
+}
