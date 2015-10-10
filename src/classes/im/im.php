48139d9774ab86851c6bff6b8f28bb0027903028
Mesilov Maxim <mesilov.maxim@gmail.com>
2015-10-11 02:33:41 +0300

    add presets to CRM\Deal entity, update Im class

--- src/classes/im/im.php
+++ src/classes/im/im.php
@@ -2,6 +2,7 @@
 namespace Bitrix24\Im;
 use Bitrix24\Bitrix24Entity;
 use Bitrix24\Bitrix24Exception;
+use Bitrix24\Presets\Im\Fields as B24ImFields;
 
 /**
  * Class Im
@@ -11,12 +12,16 @@ class Im extends Bitrix24Entity
 {
 	/**
 	 * send notification to user
+	 * @link https://training.bitrix24.com/rest_help/im/im_notify.php
 	 * @param $userId integer bitrix24 user identifier
 	 * @param $message string message to user, support some html tags
+	 * @param $notifyType string
+	 *  - SYSTEM send message from current application
+	 *  - USER send message from current user, default value
 	 * @return array
 	 * @throws Bitrix24Exception
 	 */
-	public function notify($userId, $message)
+	public function notify($userId, $message, $notifyType = B24ImFields::NOTIFY_TYPE_USER)
 	{
 		if(is_null($userId))
 		{
@@ -26,12 +31,16 @@ class Im extends Bitrix24Entity
 		{
 			throw new Bitrix24Exception('message is null');
 		}
-
+		elseif(!in_array(strtoupper($notifyType), array(B24ImFields::NOTIFY_TYPE_SYSTEM, B24ImFields::NOTIFY_TYPE_USER), true))
+		{
+			throw new Bitrix24Exception('unknown notifyType');
+		}
 		$fullResult = $this->client->call(
 			'im.notify',
 			array(
 				'to' => $userId,
-				'message' => $message
+				'message' => $message,
+				'type' => strtoupper($notifyType)
 			)
 		);
 		return $fullResult;
---
+++ src/presets/im/fields.php
@@ -0,0 +1,17 @@
+<?php
+namespace Bitrix24\Presets\Im;
+/**
+ * Class Fields
+ * @package Bitrix24\Presets\Im
+ */
+class Fields
+{
+	/**
+	 * @var string
+	 */
+	const NOTIFY_TYPE_USER = 'USER';
+	/**
+	 * @var string
+	 */
+	const NOTIFY_TYPE_SYSTEM = 'SYSTEM';
+}
