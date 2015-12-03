c814615879a91e6a97c1d548bd5e962eeb639989
yugene <yugene@monulent.ru>
2015-12-04 00:15:53 +0300

    - crm.deal.add params - crm.lead.add params

--- src/classes/crm/lead/lead.php
+++ src/classes/crm/lead/lead.php
@@ -45,14 +45,19 @@ class Lead extends Bitrix24Entity
 	/**
 	 * Add a new lead to CRM
 	 * @param array $fields array of fields
+     * @param array $params Set of parameters. REGISTER_SONET_EVENT - performs registration of a change event in a lead in the Activity Stream.
+     * The lead's Responsible person will also receive notification.
 	 * @link http://dev.1c-bitrix.ru/rest_help/crm/leads/crm_lead_add.php
 	 * @return array
 	 */
-	public function add($fields = array())
+	public function add($fields = array(), $params = array())
 	{
 		$fullResult = $this->client->call(
 			'crm.lead.add',
-			array('fields' => $fields)
+			array(
+                'fields' => $fields,
+                'params' => $params
+            )
 		);
 		return $fullResult;
 	}
