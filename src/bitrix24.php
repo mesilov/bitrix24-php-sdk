1747c3e564e73699e08112d7be327c3bc2594947
Mesilov Maxim <mesilov.maxim@gmail.com>
2016-08-05 00:26:08 +0300

    task#47 Update scope presets class

--- src/bitrix24.php
+++ src/bitrix24.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the bitrix24-php-sdk package.
+ *
+ * © Mesilov Maxim <mesilov.maxim@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Bitrix24;
 
 use Bitrix24\Contracts\iBitrix24;
--- src/presets/scope.php
+++ src/presets/scope.php
@@ -8,20 +8,90 @@
  * file that was distributed with this source code.
  */
 namespace Bitrix24\Presets;
+
 /**
  * Class Scope
  * @package Bitrix24\Presets
  */
 class Scope
 {
-	const DEPARTMENT = 'department';
-	const CRM = 'crm';
-	const CALENDAR = 'calendar';
-	const USER = 'user';
-	const ENTITY = 'entity';
-	const TASK = 'task';
-	const TASKS_EXTENDED = 'tasks_extended';
-	const IM = 'im';
-	const LOG = 'log';
-	const SONET_GROUP = 'sonet_group';
+    /**
+     * @var string work with business process
+     */
+    const BIZPROC = 'bizproc';
+
+    /**
+     * @var string work with calendar
+     */
+    const CALENDAR = 'calendar';
+
+    /**
+     * @var string work with CRM
+     */
+    const CRM = 'crm';
+
+    /**
+     * @var string work with bitrix24 disk
+     */
+    const DISK = 'disk';
+
+    /**
+     * @var string work with departments
+     */
+    const DEPARTMENT = 'department';
+
+    /**
+     * @var string work with B24 entity
+     */
+    const ENTITY = 'entity';
+
+    /**
+     * @var string work with IM
+     */
+    const IM = 'im';
+
+    /**
+     * @var string work with IM bot
+     */
+    const IMBOT = 'imbot';
+
+    /**
+     * @var string work with users
+     */
+    const USER = 'user';
+
+    /**
+     * @var string work with mail
+     */
+    const MAILSERVICE = 'mailservice';
+
+    /**
+     * @var string work with task
+     */
+    const TASK = 'task';
+
+    /**
+     * @var string work with task extended
+     */
+    const TASKS_EXTENDED = 'tasks_extended';
+
+    /**
+     * @var string work with telephony
+     */
+    const TELEPHONY = 'telephony';
+
+    /**
+     * @var string work with life line
+     */
+    const LOG = 'log';
+
+    /**
+     * @var string work with sonet group
+     */
+    const SONET_GROUP = 'sonet_group';
+
+    /**
+     * @var string work with lists
+     */
+    const LISTS = 'lists';
 }
