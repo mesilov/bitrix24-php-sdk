1bec511360fe867a3d671cf3e99082bcf6a5af2d
Boudewijn van Breukelen <b@vbreukelen.com>
2016-09-02 16:59:08 +0200

    Added if isset to catch no errors

--- src/bitrix24.php
+++ src/bitrix24.php
@@ -877,7 +877,7 @@ class Bitrix24 implements iBitrix24
         $url = 'https://' . $domain . '/rest/app.info?auth=' . $accessToken;
         $requestResult = $this->executeRequest($url);
         if (isset($requestResult['error'])) {
-            if (in_array($requestResult['error'], array('EXPIRED_TOKEN', 'INVALID_TOKEN', 'WRONG_TOKEN'), false)) {
+            if (in_array($requestResult['error'], array('expired_token', 'invalid_token', 'WRONG_TOKEN'), false)) {
                 $isTokenExpire = true;
             } else {
                 // handle other errors
