90bc6bbc15c763e2bb31761a4b4dfd0ed32c047f
Boudewijn van Breukelen <b@vbreukelen.com>
2016-09-02 14:38:55 +0200

    Fixed two errors on line 879 in isAccessTokenExpire method

--- src/bitrix24.php
+++ src/bitrix24.php
@@ -876,7 +876,7 @@ class Bitrix24 implements iBitrix24
 //		$url = 'https://'.self::OAUTH_SERVER.'/rest/app.info?auth='.$accessToken;
         $url = 'https://' . $domain . '/rest/app.info?auth=' . $accessToken;
         $requestResult = $this->executeRequest($url);
-        if (in_array($requestResult['error'], array('expired_token', 'invalid_token', 'WRONG_TOKEN'), false)) {
+        if (isset($requestResult['error']) && in_array($requestResult['error'], array('EXPIRED_TOKEN', 'INVALID_TOKEN', 'WRONG_TOKEN'), false)) {
             $isTokenExpire = true;
         } else {
             // handle other errors
