369627ac66a3567351969eafb289da7b22c85b2d
yugene <e-egorov@yandex.ru>
2017-09-28 01:05:29 +0300

    Deal categories (fields, list, stages)

---
+++ src/classes/crm/dealcategory/dealcategory.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Bitrix24\CRM\DealCategory;
+
+use Bitrix24\Bitrix24Entity;
+
+class DealCategory extends Bitrix24Entity
+{
+    /**
+     * Get list of deal category fields with description
+     *
+     * @link https://dev.1c-bitrix.ru/rest_help/crm/category/crm_dealcategory_fields.php
+     * @return array
+     */
+    public function fields()
+    {
+        $fullResult = $this->client->call(
+            'crm.dealcategory.fields'
+        );
+
+        return $fullResult;
+    }
+
+    /**
+     * Get list of deal categories.
+     *
+     * @link https://dev.1c-bitrix.ru/rest_help/crm/category/crm_dealcategory_list.php
+     * @param array $order - order of deal category items
+     * @param array $filter - filter array
+     * @param array $select - array of columns to select
+     * @param integer $start - entity number to start from (usually returned in 'next' field of previous 'crm.dealcategory.list' API call)
+     * @return array
+     */
+    public function getList($order = [], $filter = [], $select = [], $start = 0)
+    {
+        $fullResult = $this->client->call(
+            'crm.dealcategory.list',
+            [
+                'order'  => $order,
+                'filter' => $filter,
+                'select' => $select,
+                'start'  => $start,
+            ]
+        );
+
+        return $fullResult;
+    }
+}
---
+++ src/classes/crm/dealcategory/stage.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Bitrix24\CRM\DealCategory;
+
+use Bitrix24\Bitrix24Entity;
+
+class Stage extends Bitrix24Entity
+{
+    /**
+     * Get list of deal category stages.
+     *
+     * @link https://dev.1c-bitrix.ru/rest_help/crm/category/crm_dealcategory_stage_list.php
+     * @param int $id - deal category id
+     * @return array
+     */
+    public function getList($id)
+    {
+        $fullResult = $this->client->call(
+            'crm.dealcategory.stage.list',
+            [
+                'id' => $id,
+            ]
+        );
+
+        return $fullResult;
+    }
+}
