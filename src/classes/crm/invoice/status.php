0082cd7e5900ff86c205dd9be5a15ddb26cc4ea6
camaxtly <camaxtly@gmail.com>
2018-02-18 14:36:08 +0300

    task#98 Add userfields support

--- src/classes/crm/invoice/status.php
+++ src/classes/crm/invoice/status.php
@@ -6,22 +6,37 @@ class Status extends Bitrix24Entity
 {
     /**
      * Get list of Invoice.Status items.
+     *
      * @link http://dev.1c-bitrix.ru/rest_help/crm/invoice_status/crm_invoice_status_list.php
-     * @param array $order - order of items
-     * @param array $filter - filter array
-     * @param array $select - array of columns to select
-	 * @param integer $start - entity number to start from (usually returned in 'next' field of previous 'crm.invoice.status.list' API call)
+     *
+     * @param array   $order  - order of items
+     * @param array   $filter - filter array
+     * @param array   $select - array of columns to select
+     * @param integer $start  - entity number to start from (usually returned in 'next' field of previous 'crm.invoice.status.list' API call)
+     *
      * @return array
+     * @throws \Bitrix24\Exceptions\Bitrix24ApiException
+     * @throws \Bitrix24\Exceptions\Bitrix24EmptyResponseException
+     * @throws \Bitrix24\Exceptions\Bitrix24Exception
+     * @throws \Bitrix24\Exceptions\Bitrix24IoException
+     * @throws \Bitrix24\Exceptions\Bitrix24MethodNotFoundException
+     * @throws \Bitrix24\Exceptions\Bitrix24PaymentRequiredException
+     * @throws \Bitrix24\Exceptions\Bitrix24PortalDeletedException
+     * @throws \Bitrix24\Exceptions\Bitrix24PortalRenamedException
+     * @throws \Bitrix24\Exceptions\Bitrix24SecurityException
+     * @throws \Bitrix24\Exceptions\Bitrix24TokenIsExpiredException
+     * @throws \Bitrix24\Exceptions\Bitrix24TokenIsInvalidException
+     * @throws \Bitrix24\Exceptions\Bitrix24WrongClientException
      */
     public function getList($order = array(), $filter = array(), $select = array(), $start = 0)
     {
         $fullResult = $this->client->call(
             'crm.invoice.status.list',
             array(
-                'order' => $order,
-                'filter'=> $filter,
-                'select'=> $select,
-		'start' => $start
+                'order'  => $order,
+                'filter' => $filter,
+                'select' => $select,
+                'start'  => $start
             )
         );
         return $fullResult;
@@ -29,10 +44,25 @@ class Status extends Bitrix24Entity
 
     /**
      * get by id
+     *
      * @link http://dev.1c-bitrix.ru/rest_help/crm/invoice/crm_invoice_status_get.php
-	 * @param integer $invoiceStatusId - invoice status identifier
+     *
+     * @param integer $invoiceStatusId - invoice status identifier
+     *
      * @return array
-     */
+     * @throws \Bitrix24\Exceptions\Bitrix24ApiException
+     * @throws \Bitrix24\Exceptions\Bitrix24EmptyResponseException
+     * @throws \Bitrix24\Exceptions\Bitrix24Exception
+     * @throws \Bitrix24\Exceptions\Bitrix24IoException
+     * @throws \Bitrix24\Exceptions\Bitrix24MethodNotFoundException
+     * @throws \Bitrix24\Exceptions\Bitrix24PaymentRequiredException
+     * @throws \Bitrix24\Exceptions\Bitrix24PortalDeletedException
+     * @throws \Bitrix24\Exceptions\Bitrix24PortalRenamedException
+     * @throws \Bitrix24\Exceptions\Bitrix24SecurityException
+     * @throws \Bitrix24\Exceptions\Bitrix24TokenIsExpiredException
+     * @throws \Bitrix24\Exceptions\Bitrix24TokenIsInvalidException
+     * @throws \Bitrix24\Exceptions\Bitrix24WrongClientException
+*/
     public function get($invoiceStatusId)
     {
         $fullResult = $this->client->call(
--- src/classes/crm/invoice/userfield.php
+++ src/classes/crm/invoice/userfield.php
@@ -1,31 +1,21 @@
 <?php
-namespace Bitrix24\CRM;
+
+namespace Bitrix24\CRM\Invoice;
+
 use Bitrix24\Bitrix24Entity;
 
-class Invoice extends Bitrix24Entity
+/**
+ * Class UserField
+ */
+class UserField extends Bitrix24Entity
 {
     /**
-     * @var string STATUS_DRAFT pre-defined invoice status "draft"
-     */
-    const STATUS_DRAFT = 'N';
-    /**
-     * @var string STATUS_PAID pre-defined invoice status "paid"
-     */
-    const STATUS_PAID = 'P';
-    /**
-     * @var string STATUS_REJECTED pre-defined invoice status "rejected"
-     */
-    const STATUS_REJECTED = 'D';
-
-    /**
-     * Get list of lead items.
+     * Get list of user fields items.
      *
-     * @link http://dev.1c-bitrix.ru/rest_help/crm/invoice/crm_invoice_list.php
+     * @link https://dev.1c-bitrix.ru/rest_help/crm/invoice/crm_invoice_userfield_list.php
      *
-     * @param array   $order  - order of task items
-     * @param array   $filter - filter array
-     * @param array   $select - array of collumns to select
-     * @param integer $start  - entity number to start from (usually returned in 'next' field of previous 'crm.invoice.list' API call)
+     * @param array $order  - order of task items
+     * @param array $filter - filter array
      *
      * @return array
      * @throws \Bitrix24\Exceptions\Bitrix24ApiException
@@ -41,25 +31,26 @@ class Invoice extends Bitrix24Entity
      * @throws \Bitrix24\Exceptions\Bitrix24TokenIsInvalidException
      * @throws \Bitrix24\Exceptions\Bitrix24WrongClientException
      */
-    public function getList($order = array(), $filter = array(), $select = array(), $start = 0)
+    public function getList($order = array(), $filter = array())
     {
         $fullResult = $this->client->call(
-            'crm.invoice.list',
+            'crm.invoice.userfield.list',
             array(
                 'order'  => $order,
                 'filter' => $filter,
-                'select' => $select,
-                'start'  => $start
             )
         );
+
         return $fullResult;
     }
 
     /**
-     * get invoice by id
+     * Get item userfield
      *
-     * @link http://dev.1c-bitrix.ru/rest_help/crm/invoice/crm_invoice_get.php
-     * @var $invoiceId integer invoice identifier
+     * @link https://dev.1c-bitrix.ru/rest_help/crm/invoice/crm_invoice_userfield_get.php
+     *
+     * @param integer $userfieldId - invoice userfield id
+     *
      * @return array
      * @throws \Bitrix24\Exceptions\Bitrix24ApiException
      * @throws \Bitrix24\Exceptions\Bitrix24EmptyResponseException
@@ -73,21 +64,24 @@ class Invoice extends Bitrix24Entity
      * @throws \Bitrix24\Exceptions\Bitrix24TokenIsExpiredException
      * @throws \Bitrix24\Exceptions\Bitrix24TokenIsInvalidException
      * @throws \Bitrix24\Exceptions\Bitrix24WrongClientException
-*/
-    public function get($invoiceId)
+     */
+    public function get($userfieldId)
     {
         $fullResult = $this->client->call(
-            'crm.invoice.get',
-            array('id' => $invoiceId)
+            'crm.invoice.userfield.get',
+            array('id' => $userfieldId)
         );
+
         return $fullResult;
     }
 
     /**
-     * delete invoice by id
+     * Delete userfield
      *
-     * @link http://dev.1c-bitrix.ru/rest_help/crm/invoice/crm_invoice_delete.php
-     * @var $invoiceId integer invoice identifier
+     * @link https://dev.1c-bitrix.ru/rest_help/crm/invoice/crm_invoice_userfield_delete.php
+     *
+     * @param integer $userfieldId - invoice userfield id
+     *
      * @return array
      * @throws \Bitrix24\Exceptions\Bitrix24ApiException
      * @throws \Bitrix24\Exceptions\Bitrix24EmptyResponseException
@@ -101,18 +95,19 @@ class Invoice extends Bitrix24Entity
      * @throws \Bitrix24\Exceptions\Bitrix24TokenIsExpiredException
      * @throws \Bitrix24\Exceptions\Bitrix24TokenIsInvalidException
      * @throws \Bitrix24\Exceptions\Bitrix24WrongClientException
-*/
-    public function delete($invoiceId)
+     */
+    public function delete($userfieldId)
     {
         $fullResult = $this->client->call(
-            'crm.invoice.delete',
-            array('id' => $invoiceId)
+            'crm.invoice.userfield.delete',
+            array('id' => $userfieldId)
         );
+
         return $fullResult;
     }
 
     /**
-     * Add a new invoice to CRM
+     * Add a new userfield to invoice
      *
      * @param array $fields array of fields
      *
@@ -129,24 +124,23 @@ class Invoice extends Bitrix24Entity
      * @throws \Bitrix24\Exceptions\Bitrix24TokenIsExpiredException
      * @throws \Bitrix24\Exceptions\Bitrix24TokenIsInvalidException
      * @throws \Bitrix24\Exceptions\Bitrix24WrongClientException
-     * @link http://dev.1c-bitrix.ru/rest_help/crm/invoice/crm_invoice_add.php
-*/
+     * @link https://dev.1c-bitrix.ru/rest_help/crm/invoice/crm_invoice_userfield_add.php
+     */
     public function add($fields = array())
     {
         $fullResult = $this->client->call(
-            'crm.invoice.add',
+            'crm.invoice.userfield.add',
             array('fields' => $fields)
         );
+
         return $fullResult;
     }
 
     /**
-     * update invoice by id
+     * Update a new userfield to invoice
      *
-     * @link http://dev.1c-bitrix.ru/rest_help/crm/invoice/crm_invoice_update.php
-     *
-     * @param $invoiceId
-     * @param $invoiceFields
+     * @param int   $userfieldId invoice userfield id
+     * @param array $fields      array of fields
      *
      * @return array
      * @throws \Bitrix24\Exceptions\Bitrix24ApiException
@@ -161,42 +155,18 @@ class Invoice extends Bitrix24Entity
      * @throws \Bitrix24\Exceptions\Bitrix24TokenIsExpiredException
      * @throws \Bitrix24\Exceptions\Bitrix24TokenIsInvalidException
      * @throws \Bitrix24\Exceptions\Bitrix24WrongClientException
-*/
-    public function update($invoiceId, $invoiceFields)
+     * @link https://dev.1c-bitrix.ru/rest_help/crm/invoice/crm_invoice_userfield_update.php
+     */
+    public function update($userfieldId, $fields = array())
     {
         $fullResult = $this->client->call(
-            'crm.invoice.update',
+            'crm.invoice.userfield.update',
             array(
-                'id' => $invoiceId,
-                'fields' => $invoiceFields
+                'id'     => $userfieldId,
+                'fields' => $fields,
             )
         );
-        return $fullResult;
-    }
 
-    /**
-     * get list of invoice fields with description
-     *
-     * @link http://dev.1c-bitrix.ru/rest_help/crm/invoice/crm_invoice_fields.php
-     * @return array
-     * @throws \Bitrix24\Exceptions\Bitrix24ApiException
-     * @throws \Bitrix24\Exceptions\Bitrix24EmptyResponseException
-     * @throws \Bitrix24\Exceptions\Bitrix24Exception
-     * @throws \Bitrix24\Exceptions\Bitrix24IoException
-     * @throws \Bitrix24\Exceptions\Bitrix24MethodNotFoundException
-     * @throws \Bitrix24\Exceptions\Bitrix24PaymentRequiredException
-     * @throws \Bitrix24\Exceptions\Bitrix24PortalDeletedException
-     * @throws \Bitrix24\Exceptions\Bitrix24PortalRenamedException
-     * @throws \Bitrix24\Exceptions\Bitrix24SecurityException
-     * @throws \Bitrix24\Exceptions\Bitrix24TokenIsExpiredException
-     * @throws \Bitrix24\Exceptions\Bitrix24TokenIsInvalidException
-     * @throws \Bitrix24\Exceptions\Bitrix24WrongClientException
-*/
-    public function fields()
-    {
-        $fullResult = $this->client->call(
-            'crm.invoice.fields'
-        );
         return $fullResult;
     }
 }
