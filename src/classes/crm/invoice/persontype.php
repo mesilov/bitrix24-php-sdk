d45008735bc5f0467ec506aa105e972b297d25db
camaxtly <camaxtly@gmail.com>
2018-02-18 15:07:33 +0300

    task#98 Add paysystem adm persontype support

---
+++ src/classes/crm/invoice/persontype.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Bitrix24\CRM\Invoice;
+
+use Bitrix24\Bitrix24Entity;
+
+/**
+ * Class PersonType
+ */
+class PersonType extends Bitrix24Entity
+{
+    /**
+     * @link https://dev.1c-bitrix.ru/rest_help/crm/invoice/crm_persontype_list.php
+     *
+     * @param array $order  - order of task items
+     * @param array $filter - filter array
+     *
+     * @return array
+     *
+     * @throws \Bitrix24\Exceptions\Bitrix24ApiException
+     * @throws \Bitrix24\Exceptions\Bitrix24EmptyResponseException
+     * @throws \Bitrix24\Exceptions\Bitrix24Exception
+     * @throws \Bitrix24\Exceptions\Bitrix24IoException
+     * @throws \Bitrix24\Exceptions\Bitrix24MethodNotFoundException
+     * @throws \Bitrix24\Exceptions\Bitrix24PaymentRequiredException
+     * @throws \Bitrix24\Exceptions\Bitrix24PortalDeletedException
+     * @throws \Bitrix24\Exceptions\Bitrix24PortalRenamedException
+     * @throws \Bitrix24\Exceptions\Bitrix24SecurityException
+     * @throws \Bitrix24\Exceptions\Bitrix24TokenIsExpiredException
+     * @throws \Bitrix24\Exceptions\Bitrix24TokenIsInvalidException
+     * @throws \Bitrix24\Exceptions\Bitrix24WrongClientException
+     */
+    public function getList($order = array(), $filter = array())
+    {
+        $fullResult = $this->client->call(
+            'crm.persontype.list',
+            array(
+                'order'  => $order,
+                'filter' => $filter,
+            )
+        );
+
+        return $fullResult;
+    }
+
+
+    /**
+     * @link https://dev.1c-bitrix.ru/rest_help/crm/invoice/crm_persontype_fields.php
+     *
+     * @return array
+     *
+     * @throws \Bitrix24\Exceptions\Bitrix24ApiException
+     * @throws \Bitrix24\Exceptions\Bitrix24EmptyResponseException
+     * @throws \Bitrix24\Exceptions\Bitrix24Exception
+     * @throws \Bitrix24\Exceptions\Bitrix24IoException
+     * @throws \Bitrix24\Exceptions\Bitrix24MethodNotFoundException
+     * @throws \Bitrix24\Exceptions\Bitrix24PaymentRequiredException
+     * @throws \Bitrix24\Exceptions\Bitrix24PortalDeletedException
+     * @throws \Bitrix24\Exceptions\Bitrix24PortalRenamedException
+     * @throws \Bitrix24\Exceptions\Bitrix24SecurityException
+     * @throws \Bitrix24\Exceptions\Bitrix24TokenIsExpiredException
+     * @throws \Bitrix24\Exceptions\Bitrix24TokenIsInvalidException
+     * @throws \Bitrix24\Exceptions\Bitrix24WrongClientException
+     *
+     */
+    public function fields()
+    {
+        $fullResult = $this->client->call(
+            'crm.persontype.fields',
+            array()
+        );
+
+        return $fullResult;
+    }
+}
