a199411461fc8f696bbfbcd2541b64d7e10792f0
camaxtly <camaxtly@gmail.com>
2018-03-25 23:59:45 +0300

    task#98 externalLink

--- src/classes/crm/invoice/invoice.php
+++ src/classes/crm/invoice/invoice.php
@@ -199,4 +199,34 @@ class Invoice extends Bitrix24Entity
         );
         return $fullResult;
     }
+
+    /**
+     * get external link for invoice
+     *
+     * @param $id
+     *
+     * @return array
+     * @throws \Bitrix24\Exceptions\Bitrix24ApiException
+     * @throws \Bitrix24\Exceptions\Bitrix24EmptyResponseException
+     * @throws \Bitrix24\Exceptions\Bitrix24Exception
+     * @throws \Bitrix24\Exceptions\Bitrix24IoException
+     * @throws \Bitrix24\Exceptions\Bitrix24MethodNotFoundException
+     * @throws \Bitrix24\Exceptions\Bitrix24PaymentRequiredException
+     * @throws \Bitrix24\Exceptions\Bitrix24PortalDeletedException
+     * @throws \Bitrix24\Exceptions\Bitrix24PortalRenamedException
+     * @throws \Bitrix24\Exceptions\Bitrix24SecurityException
+     * @throws \Bitrix24\Exceptions\Bitrix24TokenIsExpiredException
+     * @throws \Bitrix24\Exceptions\Bitrix24TokenIsInvalidException
+     * @throws \Bitrix24\Exceptions\Bitrix24WrongClientException
+     */
+    public function getExternalLink($id)
+    {
+        $fullResult = $this->client->call(
+            'crm.invoice.getexternallink',
+            array(
+                'id' => $id
+            )
+        );
+        return $fullResult;
+    }
 }
