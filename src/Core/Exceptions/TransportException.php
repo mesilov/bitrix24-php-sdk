5c1395e6ccbe4f80844c1502b2b30b95cf614c99
mesilov <mesilov.maxim@gmail.com>
2020-12-06 00:22:21 +0300

    add TransportException

---
+++ src/Core/Exceptions/TransportException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bitrix24\SDK\Core\Exceptions;
+
+/**
+ * When any error happens at the transport level.
+ *
+ * Class TransportException
+ *
+ * @package Bitrix24\SDK\Core\Exceptions
+ */
+class TransportException extends BaseException
+{
+}
