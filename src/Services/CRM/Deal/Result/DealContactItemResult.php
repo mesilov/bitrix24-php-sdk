7ca7d683255c394d74972d2dbf84ee90e2a6e07f
mesilov <mesilov.maxim@gmail.com>
2021-02-01 01:33:54 +0300

    add deal contact service and test

---
+++ src/Services/CRM/Deal/Result/DealContactItemResult.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bitrix24\SDK\Services\CRM\Deal\Result;
+
+use Bitrix24\SDK\Core\Result\AbstractItem;
+
+/**
+ * Class DealItemResult
+ *
+ * @property-read int    $CONTACT_ID
+ * @property-read int    $SORT
+ * @property-read int    $ROLE_ID
+ * @property-read string $IS_PRIMARY
+ */
+class DealContactItemResult extends AbstractItem
+{
+}
