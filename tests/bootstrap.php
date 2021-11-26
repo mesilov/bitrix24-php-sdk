7ca1ff5ab2233d282b15015f7afaa915daf6a433
mesilov <mesilov.maxim@gmail.com>
2021-11-27 00:23:48 +0300

    fix errors on test suites
    Signed-off-by: mesilov <mesilov.maxim@gmail.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,4 +2,22 @@
 
 declare(strict_types=1);
 
-require_once \dirname(__DIR__) . '/vendor/autoload.php';
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Dotenv\Dotenv;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+if (!class_exists(Dotenv::class)) {
+    throw new LogicException('You need to add "symfony/dotenv" as Composer dependencies.');
+}
+
+$input = new ArgvInput();
+if (null !== $env = $input->getParameterOption(['--env', '-e'], null, true)) {
+    putenv('APP_ENV=' . $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = $env);
+}
+
+if ($input->hasParameterOption('--no-debug', true)) {
+    putenv('APP_DEBUG=' . $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = '0');
+}
+
+(new Dotenv())->bootEnv(dirname(__DIR__) .'/tests/.env.local');
