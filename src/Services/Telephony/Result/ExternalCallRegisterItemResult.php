1d2b4479ba1217b6dc28e9324d9617952f29f33d
Кирилл Храмов <hramovkirill01@gmail.com>
2022-07-11 19:13:18 +0300

    -Обновлен метод telephony.externalcall.register -Добавлен файл CONTRIBUTING.md(не редактировался)

--- src/Services/Telephony/Result/ExternalCallRegisterItemResult.php
+++ src/Services/Telephony/Result/ExternalCallRegisterItemResult.php
@@ -7,12 +7,12 @@ namespace Bitrix24\SDK\Services\Telephony\Result;
 use Bitrix24\SDK\Core\Result\AbstractItem;
 
 /**
- * @property-read  string $callId
- * @property-read  int $crmCreatedLead
- * @property-read  int $crmEntityId
- * @property-read  string $crmEntityTye
- * @property-read  array $crmCreatedEntities
- * @property-read  string $leadCreationError
+ * @property-read  string $CALL_ID
+ * @property-read  int $CRM_CREATED_LEAD
+ * @property-read  int $CRM_ENTITY_ID
+ * @property-read  string $CRM_ENTITY_TYPE
+ * @property-read  array $CRM_CREATED_ENTITIES
+ * @property-read  string $LEAD_CREATION_ERROR
  */
 class ExternalCallRegisterItemResult extends AbstractItem
 {
