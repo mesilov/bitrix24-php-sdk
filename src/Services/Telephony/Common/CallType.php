90d6c107910ca1841762e8bceb3683ecff94f17a
Кирилл Храмов <hramovkirill01@gmail.com>
2022-07-15 14:03:21 +0300

    -edit CallType.php and StatusCodeInterface.php

--- src/Services/Telephony/Common/CallType.php
+++ src/Services/Telephony/Common/CallType.php
@@ -9,39 +9,71 @@ use Bitrix24\SDK\Core\Exceptions\InvalidArgumentException;
 class CallType
 {
 
-    public const one = 1;
-    public const two = 2;
-    public const three = 3;
-    public const four = 4;
-
-    private int $code;
+    private const outboundCall = '1';
+    private const inboundCall = '2';
+    private const inboundCallWithRedirection = '3';
+    private const backCall = '4';
+    private string $code;
 
     /**
-     * @param int $code
+     * @param string $typeCode
      * @throws \Bitrix24\SDK\Core\Exceptions\InvalidArgumentException
      */
 
-    private function __construct(int $code)
+    private function __construct(string $typeCode)
     {
-        if
-        (
-            self::one !== $code &&
-            self::two !== $code &&
-            self::three !== $code &&
-            self::four !== $code
-        ) {
-            throw new InvalidArgumentException(sprintf('unknown type call %s', $code));
+        switch ($typeCode) {
+            case $this::inboundCall:
+            case $this::outboundCall:
+            case $this::inboundCallWithRedirection:
+            case $this::backCall:
+                $this->code = $typeCode;
+                break;
+            default:
+                throw new InvalidArgumentException(sprintf('unknown type call %s', $typeCode));
         }
-        $this->code = $code;
-
     }
 
     /**
-     * @return int
+     * @return string
      */
-    public function getValue(): int
+
+    public static function outboundCall(): string
+    {
+        return self::outboundCall;
+    }
+
+    /**
+     * @return string
+     */
+
+    public static function inboundCall(): string
+    {
+        return self::inboundCall;
+    }
+
+    /**
+     * @return string
+     */
+
+    public static function inboundCallWithRedirection(): string
+    {
+        return self::inboundCallWithRedirection;
+    }
+
+    /**
+     * @return string
+     */
+
+    public static function backCall(): string
+    {
+        return self::backCall;
+    }
+
+    public function __toString(): string
     {
         return $this->code;
     }
+
 }
 
--- src/Services/Telephony/Common/StatusCodeInterface.php
+++ src/Services/Telephony/Common/StatusCodeInterface.php
@@ -6,17 +6,15 @@ namespace Bitrix24\SDK\Services\Telephony\Common;
 
 interface StatusCodeInterface
 {
-    public const statusSuccessfulCall  = 'VI_STATUS_200';
-    public const statusMissedCall = 'VI_STATUS_304';
-    public const statusRejected = 'VI_STATUS_603';
-    public const statusCanceledCall = 'VI_STATUS_603-S';
-    public const statusForbidden = 'VI_STATUS_403';
-    public const statusWrongNumber = 'VI_STATUS_404';
-    public const statusBusy = 'VI_STATUS_486';
-    public const statusThisRouteIsNotAvailable = 'VI_STATUS_484';
-    public const statusTemporarilyUnavailable = 'VI_STATUS_480';
-    public const statusInsufficientFundsInTheAccount ='VI_STATUS_402';
-    public const statusBlocked = 'VI_STATUS_423';
-    public const statusNotDetermined = 'VI_STATUS_OTHER';
-
+     const STATUS_OK = 200;
+     const STATUS_NOT_MODIFIED = 304;
+     const STATUS_DECLINE = 603;
+     const STATUS_FORBIDDEN = 403;
+     const STATUS_NOT_FOUND = 404;
+     const STATUS_BUSY_HERE = 486;
+     const STATUS_ADDRESS_INCOMPLETE= 484;
+     const STATUS_SERVICE_UNAVAILABLE = 503;
+     const STATUS_TEMPORARILY_UNAVAILABLE = 480;
+     const STATUS_PAYMENT_REQUIRED = 402;
+     const STATUS_INTERVAL_TOO_BRIEF = 423;
 }
