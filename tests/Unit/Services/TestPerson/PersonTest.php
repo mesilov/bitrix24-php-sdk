ef264a96de201b7b44395e90d793cac33de00dbb
Кирилл Храмов <hramovkirill01@gmail.com>
2022-07-26 18:02:44 +0300

    -add test normalize -and edit  NetworkTimingsParser.php

--- tests/Unit/Services/TestPerson/PersonTest.php
+++ tests/Unit/Services/TestPerson/PersonTest.php
@@ -3,11 +3,17 @@ declare(strict_types=1);
 
 namespace Bitrix24\SDK\Tests\Unit\Services\TestPerson;
 
+use Money\Currency;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
+use Money\Money;
+use Money\Currencies\ISOCurrencies;
+use Money\Formatter\AggregateMoneyFormatter;
+use Money\Formatter\IntlMoneyFormatter;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 
 class PersonTest extends TestCase
@@ -25,6 +31,7 @@ class PersonTest extends TestCase
         $person = new Person();
         $person->setName('Kirill');
         $person->setLastName('Khramov');
+
 
 
         $jsonContent = $serializer->serialize($person, 'json');
@@ -54,5 +61,31 @@ class PersonTest extends TestCase
         $person = $serializer->deserialize($data, Person::class, 'xml');
       var_dump($person);
         self::assertNotEmpty($person);
+    }
+
+    /**
+     * @test
+     * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
+     */
+    public function NormalizeMoneyTest():void
+    {
+        $encoders = [new XmlEncoder(), new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $dollars = new Money(100,new Currency('USD'));
+
+        $numberFormatter = new \NumberFormatter('en_US', \NumberFormatter::CURRENCY);
+        $intlFormatter = new IntlMoneyFormatter($numberFormatter, new ISOCurrencies());
+
+        $moneyFormatter = new AggregateMoneyFormatter([
+            'USD' => $intlFormatter,
+        ]);
+
+        $money =  $moneyFormatter->format($dollars);
+        $jsonContent = $serializer->normalize($money,null, [AbstractNormalizer::ATTRIBUTES => ['amount']]);
+        var_dump($jsonContent);
+        self::assertNotEmpty($money);
     }
 }
