9009ce586b1ecde48d8e6d71c4d74079bce6a012
Кирилл Храмов <hramovkirill01@gmail.com>
2023-03-22 16:55:23 +0300

    - Апдейт без батча

--- tests/Integration/OperatingTimingTest/OperatingTimingTest.php
+++ tests/Integration/OperatingTimingTest/OperatingTimingTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Bitrix24\SDK\Tests\Integration\OperatingTimingTest;
 
+use Bitrix24\SDK\Core\Batch;
 use Bitrix24\SDK\Core\Exceptions\BaseException;
 use Bitrix24\SDK\Core\Exceptions\TransportException;
 use Bitrix24\SDK\Services\CRM\Contact\Service\Contact;
@@ -19,52 +20,51 @@ use PHPUnit\Framework\TestCase;
 class OperatingTimingTest extends TestCase
 {
     protected Contact $contactService;
+    private Batch $batch;
 
     /**
      * @throws \Bitrix24\SDK\Core\Exceptions\TransportException
      * @throws \Bitrix24\SDK\Core\Exceptions\BaseException
      */
-    public function testOperatingTiming()
+    public function testOperatingTiming(): void
     {
-        $PhoneNumWorkMas = [];
-        $PhoneNumHomeMas = [];
         $masContacts = [];
-        for($i=0; $i<5; $i++){
+        for ($i = 0; $i < 5; $i++) {
             $phoneNumberWork = sprintf('+7%s', time());
             $phoneNumberHome = sprintf('%s', microtime());
-            $phoneNumberHome = implode("-",str_split(substr($phoneNumberHome,2,-13),2));
+            $phoneNumberHome = implode("-", str_split(substr($phoneNumberHome, 2, -13), 2));
             $masContacts[] = [
-                'NAME' => sprintf('first_%s', time()),
-                'SECOND' => sprintf('second_%s', time()),
-                'PHONE' => [
-                    [
-                        'VALUE' => $phoneNumberWork,
-                        'VALUE_TYPE' => 'WORK'
+                'fields' => [
+                    'NAME' => sprintf('first_%s', time()),
+                    'SECOND' => sprintf('second_%s', time()),
+                    'PHONE' => [
+                        [
+                            'VALUE' => $phoneNumberWork,
+                            'VALUE_TYPE' => 'WORK'
+                        ],
+                        [
+                            'VALUE' => $phoneNumberHome,
+                            'VALUE_TYPE' => 'HOME'
+                        ]
                     ],
-                    [
-                        'VALUE' => $phoneNumberHome,
-                        'VALUE_TYPE' => 'HOME'
-                    ]
                 ]
             ];
 
         }
-        foreach ($masContacts as $contact){
-            var_dump($contact);
-            $contactId[] = $this->contactService->add($contact)->getId();
-            foreach ($contactId as $id){
-                $contactUpdate= $this->contactService->update($id,['PHONE'[0] => '']);
-            }
-
-            self::assertGreaterThanOrEqual(1,$contactId);
+        $contactsIdList = [];
+        foreach ($this->batch->addEntityItems('crm.contact.add', $masContacts) as $addContactResult) {
+            $contactsIdList[] = $addContactResult->getResult();
         }
-        var_dump($contactId);
-
-
+        foreach ($contactsIdList as $contactId) {
+            $contactPhoneId = $this->contactService->get($contactId[0])->contact()->PHONE[0]['ID'];
+            $contactUpdate = $this->contactService->update((int)$contactId[0],['PHONE' => [['ID' => $contactPhoneId, 'VALUE' => ""]]],[ "REGISTER_SONET_EVENT" => "Y"]);
+        }
+        $this->assertCount(5, $contactsIdList);
     }
 
     public function setUp(): void
     {
+        $this->batch = Fabric::getBatchService();
         $this->contactService = Fabric::getServiceBuilder()->getCRMScope()->contact();
     }
 }
