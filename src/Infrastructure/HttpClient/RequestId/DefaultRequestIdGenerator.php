f62e2db7e026db0fd409cdd4d79ae4f1d9186fc0
mesilov <mesilov.maxim@gmail.com>
2023-11-26 02:23:22 +0600

    first version
    Signed-off-by: mesilov <mesilov.maxim@gmail.com>

--- src/Infrastructure/HttpClient/RequestId/DefaultRequestIdGenerator.php
+++ src/Infrastructure/HttpClient/RequestId/DefaultRequestIdGenerator.php
@@ -8,15 +8,43 @@ use Symfony\Component\Uid\Uuid;
 
 class DefaultRequestIdGenerator implements RequestIdGeneratorInterface
 {
+    private const DEFAULT_REQUEST_ID_FIELD_NAME = 'X-Request-ID';
+    private const KEY_NAME_VARIANTS = [
+        'REQUEST_ID',
+        'HTTP_X_REQUEST_ID',
+        'UNIQUE_ID'
+    ];
+
+    private function generate(): string
+    {
+        return Uuid::v7()->toRfc4122();
+    }
+
+    private function findExists(): ?string
+    {
+        $candidate = null;
+        foreach(self::KEY_NAME_VARIANTS as $key)
+        {
+           if(!empty($_SERVER[$key]))
+           {
+               $candidate = $_SERVER[$key];
+               break;
+           }
+        }
+        return $candidate;
+    }
+
     public function getRequestId(): string
     {
-        // get from  server fields
-        // if empty - generate
-        return Uuid::v7()->toRfc4122();
+        $reqId = $this->findExists();
+        if ($reqId === null) {
+            $reqId = $this->generate();
+        }
+        return $reqId;
     }
 
     public function getHeaderFieldName(): string
     {
-        return 'X-Request-ID';
+        return self::DEFAULT_REQUEST_ID_FIELD_NAME;
     }
 }
